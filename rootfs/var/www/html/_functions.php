022a2285996e7e4bc65b66653de8875a65ac7c4a
realizelol <bsw.bsw@gmx.de>
2024-11-28 17:01:47 +0100

    [TEST] 16th try.

--- rootfs/var/www/html/_functions.php
+++ rootfs/var/www/html/_functions.php
@@ -4,6 +4,13 @@ if (getenv("IS_PRODUCTION")) {
   error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 }
 
+$fb_host = getenv("FRITZBOX_HOST");
+$fb_port = getenv("FRITZBOX_PORT");
+$fb_prot = getenv("FRITZBOX_PROT");
+$fb_user = getenv("FRITZBOX_USER");
+$fb_pass = getenv("FRITZBOX_PASS");
+$fb_wlan = getenv("FRITZBOX_WLAN");
+
 class fritzbox {
 
   protected $host = null;
@@ -12,6 +19,7 @@ class fritzbox {
   protected $user = null;
   protected $pass = null;
   protected $wlan = null;
+  protected $data = null;
 
   function send($fb_host = null, $fb_port = null, $fb_prot = null, $fb_user = null, $fb_pass = null, $fb_wlan = null, $fb_data = null) {
     if ($fb_host === null || $fb_user === null || $fb_pass === null || $fb_data === null)
@@ -28,50 +36,61 @@ class fritzbox {
     $this->user = $fb_user;
     $this->pass = $fb_pass;
     $this->wlan = $fb_wlan;
+    $this->data = $fb_data;
     $uri = "urn:dslforum-org:service:WLANConfiguration:".$this->wlan;
-    $location = "/upnp/control/wlanconfig".$this->wlan;
-
-    $fullurl = $this->prot."://".$this->host.":".$this->port."/login_sid.lua";
+    $location = "upnp/control/wlanconfig".$this->wlan;
+    $fullurl = $this->prot."://".$this->host.":".$this->port."/".$location;
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $fullurl);
-    //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    if ($fb_data = "guest_enable") {
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    if ($this->data == "guest_enable") {
       $tog = 1;
       $post_data = "<?xml version='1.0' encoding='utf-8'?><s:Envelope s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/' xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'><s:Body><u:SetEnable xmlns:u='".$uri."'><NewEnable>".$tog."</NewEnable></u:SetEnable></s:Body></s:Envelope>";
-      curl_setopt($ch, CURLOPT_HEADER, array(
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: text/xml; charset="utf-8"',
         'SoapAction:'.$uri.'#SetEnable'
       ));
-    } elseif ($fb_data = "guest_disable") {
+    } elseif ($this->data == "guest_disable") {
       $tog = 0;
       $post_data = "<?xml version='1.0' encoding='utf-8'?><s:Envelope s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/' xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'><s:Body><u:SetEnable xmlns:u='".$uri."'><NewEnable>".$tog."</NewEnable></u:SetEnable></s:Body></s:Envelope>";
-      curl_setopt($ch, CURLOPT_HEADER, array(
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: text/xml; charset="utf-8"',
         'SoapAction:'.$uri.'#SetEnable'
       ));
-    } elseif ($fb_data = "guest_status") {
-      curl_setopt($ch, CURLOPT_HEADER, array(
+    } elseif ($this->data == "guest_status") {
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: text/xml; charset="utf-8"',
         'SoapAction:'.$uri.'#GetInfo'
       ));
       $post_data = "<?xml version='1.0' encoding='utf-8'?><s:Envelope s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/' xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'><s:Body><u:GetInfo xmlns:u='".$uri."'></u:GetInfo></s:Body></s:Envelope>";
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     }
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-    curl_setopt($ch, CURLOPT_USERPWD, "$this->user:$this->pass");
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANYSAFE);
+    curl_setopt($ch, CURLOPT_USERPWD, $this->user.':'.$this->pass);
     $result = curl_exec($ch);
-    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
 
-    if ( $result === null) {
-      return status_code;
+    // only show the relevant part (0=off|1=on) of 'guest_status'
+    if (str_contains($result, 'GetInfoResponse')) {
+      preg_match("/(?<=<NewEnable>)[0-1]?(?=<\/NewEnable>)/", $result, $result);
+      $result = $result[0];
+    } elseif (str_contains($result, 'SetEnableResponse')) {
+      $result = 'successfully send.';
     } else {
-      return result;
+      $result = 'error sending.';
+    }
+
+    if ($result === null) {
+      return $status_code;
+    } else {
+      return $result;
     }
   }
 }
